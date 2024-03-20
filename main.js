55564d79a1ab18d269fc8e715cb34c7dd2f68fd5
Ryan <87626768+Ralac07@users.noreply.github.com>
2024-03-20 12:57:58 +1100

    wip math module

--- main.js
+++ main.js
@@ -89,5 +89,120 @@ const False = false;
 
 // https://www.w3schools.com/python/module_math.asp
 class math {
-	static acos
+	static acos(__x){
+		return Math.acos(__x);
+	}
+	static acosh(__x){
+		return Math.acosh(__x);
+	}
+	static asin(__x){
+		return Math.asin(__x);
+	}
+	static asinh(__x){
+		return Math.asinh(__x);
+	}
+	static atan(__x){
+		return Math.atan(__x);
+	}
+	static atan2(__x){
+		return Math.atan2(__x);
+	}
+	static atanh(__x){
+		return Math.atanh(__x);
+	}
+	static ceil(__x){
+		return Math.ceil(__x);
+	}
+	static comb(__x){
+		return Math.comb(__x);
+	}
+	static copysign(__x){
+		return Math.copysign(__x);
+	}
+	static cos(__x){
+		return Math.cos(__x);
+	}
+	static cosh(__x){
+		return Math.cosh(__x);
+	}
+	static degrees(__x){
+		return __x * (180/Math.PI);
+	}
+		static dist(__p,__q){
+			if (__p.length == 1){
+				__p.push(__p[0])
+			}
+			if (__q.length == 1){
+				__q.push(__p[0])
+			}
+			let x1 = __p[0];
+			let x2 = __q[0];
+			let y1 = __p[1];
+			let y2 = __q[1];
+		return Math.sqrt( Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2) );
+	}
+	static erf(x){ // https://hewgill.com/picomath/javascript/erf.js.html
+		// constants
+		var a1 =  0.254829592;
+		var a2 = -0.284496736;
+		var a3 =  1.421413741;
+		var a4 = -1.453152027;
+		var a5 =  1.061405429;
+		var p  =  0.3275911;
+	
+		// Save the sign of x
+		var sign = 1;
+		if (x < 0) {
+			sign = -1;
+		}
+		x = Math.abs(x);
+	
+		// A&S formula 7.1.26
+		var t = 1.0/(1.0 + p*x);
+		var y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*Math.exp(-x*x);
+	
+		return sign*y;
+	}
+	static erfc(){
+		throw Error("Function not implemented");
+	}
+	static exp(x){
+		return Math.E ** x;
+	}
+	static expm1(x){
+		return (Math.E ** x) - 1;
+	}
+	static fabs(__x){
+		return Math.abs(__x) * 1.0;
+	}
+	static factorial(__x){
+		let out = 1;
+		let i = 1;
+		for (i;i<=__x;i++){
+			out *= i;
+		}
+		return out;
+	}
+	static floor(__x){
+		return Math.floor(__x);
+	}
+	static fmod(x,y){
+		return x%y * 1.0
+	}
+	static frexp(value) { // https://blog.codefrau.net/2014/08/deconstructing-floats-frexp-and-ldexp.html
+		if (value === 0) return [value, 0];
+		var data = new DataView(new ArrayBuffer(8));
+		data.setFloat64(0, value);
+		var bits = (data.getUint32(0) >>> 20) & 0x7FF;
+		if (bits === 0) { // denormal
+			data.setFloat64(0, value * Math.pow(2, 64));  // exp + 64
+			bits = ((data.getUint32(0) >>> 20) & 0x7FF) - 64;
+		}
+		var exponent = bits - 1022;
+		var mantissa = ldexp(value, -exponent);
+		return [mantissa, exponent];
+	}
+	static fsum(__x){
+		//get sum
+	}
 }
---
+++ testing.py
@@ -0,0 +1,5 @@
+import math
+x=7
+y=2
+print( math.fmod(x,y) )
+print( x%y * 1.0 )
