2f75be93ecb3cca5ac1e9790ddc05ee38d5afc3d
Ryan <87626768+Ralac07@users.noreply.github.com>
2024-03-22 17:17:51 +1100

    added string.lstrip()

--- main.js
+++ main.js
@@ -613,7 +613,22 @@ String.prototype.upper = function (inputs = undefined) {
 	let theStr = this;
 	return theStr.toUpperCase();
 };
-
+String.prototype.lstrip = function (chars=" \r\n\t    "){
+    let theStr = this;
+    let output = theStr;
+    for (const [index, element] of theStr.split('').entries()) {
+        if (chars.split("").includes(element)){
+            output[index] = " ";
+            output = output.split("");
+            output[index] = " ";
+            output = output.join("");
+        } else {
+            break;
+        }
+        theStr = output.trimStart();
+    }
+    return theStr;
+}
 // String.prototype.strip = function (chars) {
 // 	let theStr = this;
 // 	this.trim()
